d623fe8a8e2fdc5ed57c0b34ed0e8a22f6615d35
Rodolfo Jordao <jordao@kth.se>
2019-01-13 17:36:12 +0100

    Tried to make it explicitly tuple-like to no avail

--- recsyde/src/Exploration/General.hs
+++ recsyde/src/Exploration/General.hs
@@ -1,9 +1,9 @@
 module Exploration.General where
 
 class Symmetric a where
-  (~=~) :: a -> a -> Bool
-  (~/~) :: a -> a -> Bool
-  (~/~) a a' = not $ a ~=~ a'
+  (~==~) :: a -> a -> Bool
+  (~/=~) :: a -> a -> Bool
+  (~/=~) a a' = not $ a ~==~ a'
 
 class Valued a where
   value :: (Ord v) => a -> v
@@ -15,34 +15,31 @@ minBy f (a:as)
   | f a > f m = m
   where m = minBy f as
 
-symmetricSets :: (Symmetric a) => [a] -> [[a]]
-symmetricSets [] = []
-symmetricSets [a] = [[a]]
-symmetricSets (a:as) = let
-  equivalents = a : [a' | a' <- as, a' ~=~ a]
-  nonequiv = [a' | a' <- as, a' ~/~ a]
+dominantSymmetricSet :: (Symmetric a, Valued a) => [(xs, a)] -> [(xs, a)]
+dominantSymmetricSet [] = []
+dominantSymmetricSet ((xs, a):xas) = let
+  equivalents = (xs, a) : [(xs', a') | (xs', a') <- xas, a' ~==~ a]
+  nonequiv = [(xs', a') | (xs', a') <- xas, a' ~/=~ a]
+  best = minBy (value . snd) equivalents
   in
-  equivalents : symmetricSets nonequiv
-
-symmetricPairSets :: (Symmetric a) => [(x, a)] -> [[(x, a)]]
-symmetricPairSets [] = []
-symmetricPairSets [(x, a)] = [[(x, a)]]
-symmetricPairSets ((x, a):xas) = let
-  equivalents = (x, a) : [(x', a') | (x', a') <- xas, a' ~=~ a]
-  nonequiv = [(x', a') | (x', a') <- xas, a' ~/~ a]
-  in
-  equivalents : symmetricPairSets nonequiv
+  best : dominantSymmetricSet nonequiv
 
 searchGoal :: (Symmetric a, Valued a) =>
   [(a -> x -> [a])] -> [([x], a)] -> [([x], a)]
 searchGoal [] _ = []
 searchGoal _ [] = []
 searchGoal rs j_k
-  | head j_kp == ([], _) = j_kp
+  | head j_k == ([], _) = j_k
   | otherwise = searchGoal rs j_kp
   where
-  s_kp = concat [(xs, a') | r <- rs, (x:xs, a) <- j_k, a' <- r a x]
-  j_kp = map minBy . (\(xs,a) -> value a) $ symmetricPairSets s_kp
-    
-  
+  j_kp = dominantSymmetricSet [(xs, a') | r <- rs, (x:xs, a) <- j_k, a' <- r a x]
+
+symmetricSets :: (Symmetric a) => [a] -> [[a]]
+symmetricSets [] = []
+symmetricSets [a] = [[a]]
+symmetricSets (a:as) = let
+  equivalents = a : [a' | a' <- as, a' ~==~ a]
+  nonequiv = [a' | a' <- as, a' ~/=~ a]
+  in
+  equivalents : symmetricSets nonequiv
 

7e68cdf49df76ee2e9301295bceb8fa9b5f716fc
jordao <jordao@kth.se>
2020-11-18 16:13:09 +0100

    Added one more minizinc model and enhanced dsi

--- python/desyder/identification.py
+++ python/desyder/identification.py
@@ -3,14 +3,15 @@ import importlib.resources as resources
 from dataclasses import dataclass, field
 from typing import Any, List, Union, Tuple, Set, Optional
 
-import minizinc
 import numpy as np
 import sympy
-from forsyde.io.python import ForSyDeModel
+from minizinc import Model as MznModel
+from minizinc import Instance as MznInstance
+from minizinc import Result as MznResult
+from forsyde.io.python import ForSyDeModel, Vertex
 
 import desyder.math as mathutil
 import desyder.sdf as sdfapi
-
 
 
 class MinizincAble(abc.ABC):
@@ -18,15 +19,22 @@ class MinizincAble(abc.ABC):
     @abc.abstractmethod
     def populate_mzn_model(
             self,
-            model: Union[minizinc.Model, minizinc.Instance]
-            ) -> Union[minizinc.Model, minizinc.Instance]:
+            model: Union[MznModel, MznInstance]
+            ) -> Union[MznModel, MznInstance]:
         return model
 
     @abc.abstractmethod
     def get_mzn_model_name(self) -> str:
         return ""
 
-    def build_mzn_model(self, model=minizinc.Model()):
+    @abc.abstractmethod
+    def rebuild_forsyde_model(
+        self,
+        result: MznResult
+    ) -> ForSyDeModel:
+        return ForSyDeModel()
+
+    def build_mzn_model(self, model=MznModel()):
         model_txt = resources.read_text(
             'desyder.minizinc',
             self.get_mzn_model_name()
@@ -56,12 +64,12 @@ class DecisionProblem(abc.ABC):
 
 
 @dataclass
-class SDFExecution(DecisionProblem, MinizincAble):
+class SDFExecution(DecisionProblem):
 
-    sdf_actors: List[str] = field(default_factory=lambda: [])
-    sdf_channels: List[str] = field(default_factory=lambda: [])
+    sdf_actors: List[Vertex] = field(default_factory=lambda: [])
+    sdf_channels: List[Vertex] = field(default_factory=lambda: [])
     sdf_topology: np.ndarray = np.zeros((0, 0))
-    sdf_repetition_vector: List[int] = field(default_factory=lambda: [])
+    sdf_repetition_vector: np.ndarray = np.zeros((0))
     sdf_pass: List[str] = field(default_factory=lambda: [])
 
     @classmethod
@@ -70,19 +78,17 @@ class SDFExecution(DecisionProblem, MinizincAble):
         :returns: TODO
 
         """
-        sdf_actors = list(
-            a['vertex_id'] for a in model.query_view('sdf_actors')
-        )
-        sdf_channels = list(
-            c['vertex_id'] for c in model.query_view('sdf_channels')
-        )
+        sdf_actors = list(a for a in model.query_vertexes('sdf_actors'))
+        sdf_channels = list(c for c in model.query_vertexes('sdf_channels'))
         sdf_topology = np.zeros(
             (len(sdf_channels), len(sdf_actors)),
             dtype=int
         )
         for row in model.query_view('sdf_topology'):
-            a_index = sdf_actors.index(row['actor_id'])
-            c_index = sdf_channels.index(row['channel_id'])
+            a_index = next(idx for (idx, v) in enumerate(sdf_actors)
+                           if v.identifier == row['actor_id'])
+            c_index = next(idx for (idx, v) in enumerate(sdf_channels)
+                           if v.identifier == row['channel_id'])
             sdf_topology[c_index, a_index] = int(row['tokens'])
         null_space = sympy.Matrix(sdf_topology).nullspace()
         if len(null_space) == 1:
@@ -107,23 +113,6 @@ class SDFExecution(DecisionProblem, MinizincAble):
         else:
             return (False, None)
 
-    def populate_mzn_model(self, model):
-        model['sdf_actors'] = range(1, len(self.sdf_actors)+1)
-        model['sdf_channels'] = range(1, len(self.sdf_channels)+1)
-        model['max_steps'] = len(self.sdf_pass)
-        cloned_firings = np.array([
-            self.repetition_vector.transpose()
-            for i in range(len(self.sdf_channels))
-        ])
-        model['max_tokens'] = np.amax(
-            cloned_firings * np.absolute(self.sdf_topology)
-        )
-        model['activations'] = self.repetition_vector
-        return model
-
-    def get_mzn_model_name(self):
-        return 'sdf_linear_dmodel.mzn'
-
 
 @dataclass
 class SDFToSlots(DecisionProblem, MinizincAble):
@@ -141,9 +130,7 @@ class SDFToSlots(DecisionProblem, MinizincAble):
             (p for p in identified if isinstance(p, SDFExecution)),
             None)
         if sdf_subproblem:
-            orderings = list(
-                r['vertex_id'] for r in model.query_view('orderings')
-            )
+            orderings = list(o for o in model.query_vertexes('orderings'))
             if orderings:
                 return (
                     True,
@@ -158,21 +145,35 @@ class SDFToSlots(DecisionProblem, MinizincAble):
             return (False, None)
 
     def populate_mzn_model(self, model):
-        self.sdf_subproblem.populate_mzn_model(model)
+        model['sdf_actors'] = range(1, len(self.sdf_actors)+1)
+        model['sdf_channels'] = range(1, len(self.sdf_channels)+1)
+        model['max_steps'] = len(self.sdf_pass)
+        cloned_firings = np.array([
+            self.repetition_vector.transpose()
+            for i in range(len(self.sdf_channels))
+        ])
+        model['max_tokens'] = np.amax(
+            cloned_firings * np.absolute(self.sdf_topology)
+        )
+        model['activations'] = self.repetition_vector
         model['static_orders'] = range(len(self.orderings))
         return model
 
     def get_mzn_model_name(self):
         return 'sdf_order_linear_dmodel.mzn'
 
+    def rebuild_forsyde_model(self, results):
+        print(results['send'])
+        print(results['mapped'])
+        return ForSyDeModel()
+
 
 @dataclass
 class SDFToSlotMultiCore(DecisionProblem, MinizincAble):
 
     sdf_to_slots_sub: SDFToSlots
-    cpus: List[str] = field(default_factory=lambda: [])
-    bus: Optional[str] = None
-    bus_slots: Optional[int] = None
+    cpus: List[Vertex] = field(default_factory=lambda: [])
+    bus: Optional[Vertex] = None
 
     @classmethod
     def identify(cls, model, identified):
@@ -185,25 +186,16 @@ class SDFToSlotMultiCore(DecisionProblem, MinizincAble):
             None)
         if sdf_to_slot_sub:
             cpus = list(
-                r['vertex_id'] for r in
-                model.query_view('tdma_mpsoc_processing_units')
+                c for c in model.query_vertexes('tdma_mpsoc_processing_units')
             )
-            bus = next((
-                r['vertex_id'] for r in
-                model.query_view('tdma_mpsoc_bus')
-            ), None)
-            bus_slots = next((
-                r['slots'] for r in
-                model.query_view('tdma_mpsoc_bus_slots')
-            ), None)
-            if cpus and bus:
+            busses = [b for b in model.query_vertexes('tdma_mpsoc_bus')]
+            if cpus and len(busses) == 1:
                 return (
                     True,
                     SDFToSlotMultiCore(
                         sdf_to_slot_sub,
                         cpus,
-                        bus,
-                        bus_slots
+                        busses[0],
                     )
                 )
             else:
@@ -217,3 +209,6 @@ class SDFToSlotMultiCore(DecisionProblem, MinizincAble):
     def populate_mzn_model(self, model):
         self.sdf_to_slot_sub.populate_mzn_model(model)
         return model
+
+    def rebuild_forsyde_model(self, results):
+        return ForSyDeModel()

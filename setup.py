63158ecdf35ccca44d9fae152e18fd0a275a570e
jordao <jordao@kth.se>
2021-03-26 15:17:18 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(name='idesyde',
-      version='0.1.5',
+      version='0.1.6',
       description='Analytical Design Space Exploration for ForSyDe',
       url='http://github.com/forsyde/IDeSyDe',
       author='Rodolfo Jordao',

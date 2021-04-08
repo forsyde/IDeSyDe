6f48e26071812d9a12f7f54ea4c2577bf4e24d3e
Rodolfo Jordão <jordao@kth.se>
2021-04-08 15:14:25 +0200

    delete a lot of oldies.

--- rust/src/main.rs
+++
@@ -1,9 +0,0 @@
-use clap::App;
-
-fn main() {
-    App::new("DeSyDe(R)")
-       .version("0.1")
-       .about("ForSyDe's Design Space Exploration Tool")
-       .author("Rodolfo Jordao <jordao@kth.se>")
-       .get_matches();
-}
--- rust/src/prolog.rs
+++
@@ -1,8 +0,0 @@
-pub mod prolog {
-
-    trait Queryable {
-        fn start() -> Result<bool, String>;
-        pub fn query_goal<T>(database : Vec<Path>, goal: String) -> Result<HashMap<String, T>, String>;
-        fn stop() ->  Result<bool, String>;
-    }
-}

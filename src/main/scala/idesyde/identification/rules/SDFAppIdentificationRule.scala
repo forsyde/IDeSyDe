45d5122d4759f4ae061425fedc6bac3327938f6a
Rodolfo Jordão <jordao@kth.se>
2021-06-10 22:09:11 +0200

    Fixed libs

--- src/main/scala/idesyde/identification/rules/SDFAppIdentificationRule.scala
+++ src/main/scala/idesyde/identification/rules/SDFAppIdentificationRule.scala
@@ -1,18 +1,18 @@
 package idesyde.identification.rules
 
 import idesyde.identification.interfaces.IdentificationRule
-import idesyde.identification.models.SDFApplication
+import idesyde.identification.models.SdfApplication
 import forsyde.io.java.core.ForSyDeModel
 import idesyde.identification.interfaces.DecisionModel
 import forsyde.io.java.core.VertexTrait
 
 final case class SDFAppIdentificationRule()
-    extends IdentificationRule[SDFApplication] {
+    extends IdentificationRule[SdfApplication] {
 
   def identify(
       model: ForSyDeModel,
       identified: Set[DecisionModel]
-  ): (Boolean, Option[SDFApplication]) = {
+  ): (Boolean, Option[SdfApplication]) = {
     val sdf_actors =
       model.vertexSet.stream.filter(v => v.hasTrait(VertexTrait.SDFComb))
     val sdf_delays =

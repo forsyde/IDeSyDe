dddcb8d6bc6122efb2b8ed1b40269ec1a7743be3
jordao <jordao@kth.se>
2021-09-28 21:41:33 +0200

    EXtra MZN

--- src/main/scala-3/idesyde/identification/models/SchedulableNetworkedDigHW.scala
+++ src/main/scala-3/idesyde/identification/models/SchedulableNetworkedDigHW.scala
@@ -17,5 +17,49 @@ final case class SchedulableNetworkedDigHW (
   val coveredVertexes: Iterable[Vertex] = hardware.coveredVertexes ++
     schedulersFromPEs.values.map(_.getViewedVertex).toSet
 
+  lazy val fixedPriorityPESymmetryRelation: Set[(GenericProcessingModule, GenericProcessingModule)] =
+    for (p <- fixedPriorityPEs;
+        pp <- fixedPriorityPEs;
+        m <- hardware.storageElems
+        // check if exists one AND ONLY ONE memory element in which the path bandwidths
+        // are exactly equal.
+        if hardware.storageElems.count(mm => {
+          hardware.paths((m, p)).map(c => hardware.bandWidthBitPerSec(c, p)).sum +
+          hardware.paths((p, m)).map(c => hardware.bandWidthBitPerSec(c, p)).sum ==
+          hardware.paths((mm, pp)).map(c => hardware.bandWidthBitPerSec(c, pp)).sum +
+          hardware.paths((pp, mm)).map(c => hardware.bandWidthBitPerSec(c, pp)).sum
+        }) == 1) yield (p, pp)
+
+  lazy val timeTriggeredPESymmetryRelation: Set[(GenericProcessingModule, GenericProcessingModule)] =
+    for (p <- timeTriggeredPEs;
+        pp <- timeTriggeredPEs;
+        m <- hardware.storageElems
+        // check if exists one AND ONLY ONE memory element in which the path bandwidths
+        // are exactly equal.
+        if hardware.storageElems.count(mm => {
+          hardware.paths((m, p)).map(c => hardware.bandWidthBitPerSec(c, p)).sum +
+          hardware.paths((p, m)).map(c => hardware.bandWidthBitPerSec(c, p)).sum ==
+          hardware.paths((mm, pp)).map(c => hardware.bandWidthBitPerSec(c, pp)).sum +
+          hardware.paths((pp, mm)).map(c => hardware.bandWidthBitPerSec(c, pp)).sum
+        }) == 1) yield (p, pp)
+
+  lazy val roundRobinPESymmetryRelation: Set[(GenericProcessingModule, GenericProcessingModule)] =
+    for (p <- roundRobinPEs;
+        pp <- roundRobinPEs;
+        m <- hardware.storageElems
+        // check if exists one AND ONLY ONE memory element in which the path bandwidths
+        // are exactly equal.
+        if hardware.storageElems.count(mm => {
+          hardware.paths((m, p)).map(c => hardware.bandWidthBitPerSec(c, p)).sum +
+          hardware.paths((p, m)).map(c => hardware.bandWidthBitPerSec(c, p)).sum ==
+          hardware.paths((mm, pp)).map(c => hardware.bandWidthBitPerSec(c, pp)).sum +
+          hardware.paths((pp, mm)).map(c => hardware.bandWidthBitPerSec(c, pp)).sum
+        }) == 1) yield (p, pp)
+
+  lazy val symmetricGroups: Set[Set[GenericProcessingModule]] =
+    fixedPriorityPEs.map(pe => {
+      
+    })
+
   override val uniqueIdentifier = "SchedulableNetworkedDigHW"
 }

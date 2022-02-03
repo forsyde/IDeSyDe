9e55f7f5d5672f6b8d509f46c5893e6b501ae1c7
jordao <jordao@kth.se>
2022-02-03 22:06:45 +0100

    Missing additions for completeness

---
+++ common/src/main/scala-3/idesyde/identification/models/mixed/PeriodicTaskToSchedHW.scala
@@ -0,0 +1,20 @@
+package idesyde.identification.models.mixed
+
+import idesyde.identification.DecisionModel
+import idesyde.identification.models.workload.SimplePeriodicWorkload
+import idesyde.identification.models.platform.SchedulableNetworkedDigHW
+import forsyde.io.java.core.Vertex
+import org.apache.commons.math3.fraction.BigFraction
+
+final case class PeriodicTaskToSchedHW(
+    val taskModel: SimplePeriodicWorkload,
+    val schedHwModel: SchedulableNetworkedDigHW,
+    val worstCaseExecution: Array[Array[Option[BigFraction]]]
+) extends DecisionModel:
+
+  val coveredVertexes: Iterable[Vertex] = taskModel.coveredVertexes ++ schedHwModel.coveredVertexes
+  val uniqueIdentifier: String = "PeriodicTaskToSchedHW"
+
+
+
+end PeriodicTaskToSchedHW

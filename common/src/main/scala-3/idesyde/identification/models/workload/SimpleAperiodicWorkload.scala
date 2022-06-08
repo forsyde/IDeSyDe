1969e6b0cb439785dc8a0fadb5c100d96db7bf04
Rodolfo Jordão <jordao@kth.se>
2022-06-08 23:01:04 +0200

    added some ignores and more

--- common/src/main/scala-3/idesyde/identification/models/workload/SimpleAperiodicWorkload.scala
+++ common/src/main/scala-3/idesyde/identification/models/workload/SimpleAperiodicWorkload.scala
@@ -7,4 +7,19 @@ import forsyde.io.java.typed.viewers.impl.DataBlock
 
 final case class SimpleAperiodicWorkload()
     extends ForSyDeDecisionModel
-    with AperiodicWorkloadMixin[Task, DataBlock] {}
+    with AperiodicWorkloadMixin {
+
+  // Members declared in idesyde.identification.models.workload.AperiodicWorkloadMixin
+  def communicationGraph: org.jgrapht.Graph[Int, Long]                                       = ???
+  def instanceControlFlowGraph: org.jgrapht.Graph[(Int, Int), org.jgrapht.graph.DefaultEdge] = ???
+  def messageQueuesSizes: Array[Long]                                                        = ???
+  def numInstances: Array[Array[Int]]                                                        = ???
+  def numMessageQueues: Array[Int]                                                           = ???
+  def numTasks: Int                                                                          = ???
+  def taskSizes: Array[Long]                                                                 = ???
+
+  // Members declared in idesyde.identification.DecisionModel
+  def coveredVertexes: Iterable[SimpleAperiodicWorkload.this.VertexT] = ???
+  def uniqueIdentifier: String                                        = ???
+
+}

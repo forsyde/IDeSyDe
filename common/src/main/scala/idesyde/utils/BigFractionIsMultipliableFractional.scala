0a47b9aff9a42791a1c03a291b0c2ce81a222146
Rodolfo Jordoa <jordao@kth.se>
2022-07-26 16:26:55 +0200

    added one and zer

--- common/src/main/scala/idesyde/utils/BigFractionIsMultipliableFractional.scala
+++ common/src/main/scala/idesyde/utils/BigFractionIsMultipliableFractional.scala
@@ -8,4 +8,6 @@ class BigFractionIsMultipliableFractional
   def div(x: BigFraction, y: BigFraction): BigFraction = x.divide(y)
   def times(a: BigFraction, b: Int): BigFraction       = a.multiply(b)
   def times(a: BigFraction, b: Long): BigFraction      = a.multiply(b)
+  override def zero: BigFraction = BigFraction.ZERO
+  override def one: BigFraction = BigFraction.ONE
 }

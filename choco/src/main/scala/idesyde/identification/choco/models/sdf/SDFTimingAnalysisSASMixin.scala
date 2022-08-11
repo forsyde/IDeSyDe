5c3bc4606d70931bb25d2f31d859b243c0e888d5
Rodolfo Jordão <jordao@kth.se>
2022-08-11 21:45:45 +0200

    started a server metaphor

--- choco/src/main/scala/idesyde/identification/choco/models/sdf/SDFTimingAnalysisSASMixin.scala
+++ choco/src/main/scala/idesyde/identification/choco/models/sdf/SDFTimingAnalysisSASMixin.scala
@@ -10,31 +10,37 @@ trait SDFTimingAnalysisSASMixin extends ChocoModelMixin {
   def schedulers: Array[Int]
 
   def maxRepetitionsPerActors(actorId: Int): Int
-  def isStaticCyclic(schedulerId: Int): Boolean
 
   def actorMapping(actorId: Int)(schedulerId: Int): BoolVar
-  def startTimeOfActorFirings(actorId: Int)(instance: Int): IntVar
+  def startLatency(schedulerId: Int): IntVar
+  def slotMaxDuration(schedulerId: Int): IntVar
   def numActorsScheduledSlotsInStaticCyclic(actorId: Int)(tileId: Int)(slotId: Int): IntVar
 
+  def slotsInAll(i: Int) =
+    (0 until actors.size)
+      .map(q =>
+        (0 until schedulers.size).map(s => numActorsScheduledSlotsInStaticCyclic(i)(s)(q)).toArray
+      )
+      .toArray
+  def slots(i: Int)(j: Int) =
+    (0 until actors.size).map(numActorsScheduledSlotsInStaticCyclic(i)(j)(_)).toArray
+
   def postOnlySASOnStaticCyclicSchedulers(): Unit = {
-    def slots(i: Int)(j: Int) =
-      (0 until actors.size).map(numActorsScheduledSlotsInStaticCyclic(i)(j)(_)).toArray
     actors.zipWithIndex.foreach((a, i) => {
-      // chocoModel.sum(numActorsScheduledSlotsInStaticCyclic(i).flatten, "=", maxRepetitionsPerActors(a)).post()
+      chocoModel.sum(slotsInAll(i).flatten, "=", maxRepetitionsPerActors(a)).post()
       schedulers.zipWithIndex
-        .filter((s, j) => isStaticCyclic(s))
         .foreach((s, j) => {
           if (actors.size > 1) {
-              chocoModel
-                .min(s"0_in_${i}_${j}", (0 until actors.size).map(slots(i)(j)(_)): _*)
-                .eq(0)
-                .post()
+            chocoModel
+              .min(s"0_in_${i}_${j}", (0 until actors.size).map(slots(i)(j)(_)): _*)
+              .eq(0)
+              .post()
           }
           chocoModel.ifThenElse(
             actorMapping(i)(j),
             chocoModel.sum(slots(i)(j), ">=", 1),
             chocoModel.sum(slots(i)(j), "=", 0)
-            )
+          )
           chocoModel.atMostNValues(slots(i)(j), chocoModel.intVar(2), true).post()
           chocoModel.sum(slots(i)(j), "<=", maxRepetitionsPerActors(a)).post()
         })

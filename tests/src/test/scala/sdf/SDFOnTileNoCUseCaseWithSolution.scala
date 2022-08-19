03d6e790a7273383321b3597e5f526862e99577e
Rodolfo Jordoa <jordao@kth.se>
2022-08-19 17:40:57 +0200

    forgot test class

--- tests/src/test/scala/sdf/SDFOnTileNoCUseCaseWithSolution.scala
+++ tests/src/test/scala/sdf/SDFOnTileNoCUseCaseWithSolution.scala
@@ -649,32 +649,32 @@ class SDFOnTileNoCUseCaseWithSolution extends AnyFunSuite with LoggingMixin {
     assert(solutions.size >= 1)
   }
 
-  // test("Correct identification and DSE of Sobel to Large") {
-  //   val inputSystem = sobelSDF3.merge(large5x6PlatformModel)
-  //   val identified  = identificationHandler.identifyDecisionModels(inputSystem)
-  //   val chosen      = explorationHandler.chooseExplorersAndModels(identified)
-  //   assert(chosen.size > 0)
-  //   assert(chosen.find((_, m) => m.isInstanceOf[ChocoSDFToSChedTileHW]).isDefined)
-  //   val solutions = chosen
-  //     .flatMap((explorer, decisionModel) =>
-  //       explorer
-  //         .explore[ForSyDeSystemGraph](decisionModel)
-  //         .map(sol =>
-  //           forSyDeModelHandler
-  //             .writeModel(
-  //               inputSystem.merge(sol),
-  //               "tests/models/sdf3/results/sobel_and_large_result.fiodl"
-  //             )
-  //           forSyDeModelHandler.writeModel(
-  //             inputSystem.merge(sol),
-  //             "tests/models/sdf3/results/sobel_and_large_result_visual.kgt"
-  //           )
-  //           sol
-  //         )
-  //     )
-  //     .take(1)
-  //   assert(solutions.size >= 1)
-  // }
+  test("Correct identification and DSE of Sobel to Large") {
+    val inputSystem = sobelSDF3.merge(large5x6PlatformModel)
+    val identified  = identificationHandler.identifyDecisionModels(inputSystem)
+    val chosen      = explorationHandler.chooseExplorersAndModels(identified)
+    assert(chosen.size > 0)
+    assert(chosen.find((_, m) => m.isInstanceOf[ChocoSDFToSChedTileHW]).isDefined)
+    val solutions = chosen
+      .flatMap((explorer, decisionModel) =>
+        explorer
+          .explore[ForSyDeSystemGraph](decisionModel)
+          .map(sol =>
+            forSyDeModelHandler
+              .writeModel(
+                inputSystem.merge(sol),
+                "tests/models/sdf3/results/sobel_and_large_result.fiodl"
+              )
+            forSyDeModelHandler.writeModel(
+              inputSystem.merge(sol),
+              "tests/models/sdf3/results/sobel_and_large_result_visual.kgt"
+            )
+            sol
+          )
+      )
+      .take(1)
+    assert(solutions.size >= 1)
+  }
 
   test("Correct decision model identification of SUSAN") {
     val identified = identificationHandler.identifyDecisionModels(susanSDF3)
@@ -733,33 +733,33 @@ class SDFOnTileNoCUseCaseWithSolution extends AnyFunSuite with LoggingMixin {
     )
   }
 
-  // test("Correct identification and DSE of all and small platform") {
-  //   val identified = identificationHandler.identifyDecisionModels(appsAndSmall)
-  //   assert(identified.size > 0)
-  //   assert(identified.find(m => m.isInstanceOf[SDFToSchedTiledHW]).isDefined)
-  //   val chosen = explorationHandler.chooseExplorersAndModels(identified)
-  //   assert(chosen.size > 0)
-  //   assert(chosen.find((_, m) => m.isInstanceOf[ChocoSDFToSChedTileHW]).isDefined)
-  //   val solutions = chosen
-  //     .flatMap((explorer, decisionModel) =>
-  //       explorer
-  //         .explore[ForSyDeSystemGraph](decisionModel)
-  //         .map(sol =>
-  //           forSyDeModelHandler
-  //             .writeModel(
-  //               appsAndSmall.merge(sol),
-  //               "tests/models/sdf3/results/all_and_small_result.fiodl"
-  //             )
-  //           forSyDeModelHandler.writeModel(
-  //             appsAndSmall.merge(sol),
-  //             "tests/models/sdf3/results/all_and_small_result_visual.kgt"
-  //           )
-  //           sol
-  //         )
-  //     )
-  //     .take(1)
-  //   assert(solutions.size >= 1)
-  // }
+  test("Correct identification and DSE of all and small platform") {
+    val identified = identificationHandler.identifyDecisionModels(appsAndSmall)
+    assert(identified.size > 0)
+    assert(identified.find(m => m.isInstanceOf[SDFToSchedTiledHW]).isDefined)
+    val chosen = explorationHandler.chooseExplorersAndModels(identified)
+    assert(chosen.size > 0)
+    assert(chosen.find((_, m) => m.isInstanceOf[ChocoSDFToSChedTileHW]).isDefined)
+    val solutions = chosen
+      .flatMap((explorer, decisionModel) =>
+        explorer
+          .explore[ForSyDeSystemGraph](decisionModel)
+          .map(sol =>
+            forSyDeModelHandler
+              .writeModel(
+                appsAndSmall.merge(sol),
+                "tests/models/sdf3/results/all_and_small_result.fiodl"
+              )
+            forSyDeModelHandler.writeModel(
+              appsAndSmall.merge(sol),
+              "tests/models/sdf3/results/all_and_small_result_visual.kgt"
+            )
+            sol
+          )
+      )
+      .take(1)
+    assert(solutions.size >= 1)
+  }
 
   test("Correct identification and DSE of all and large platform") {
     val identified = identificationHandler.identifyDecisionModels(appsAndLarge)

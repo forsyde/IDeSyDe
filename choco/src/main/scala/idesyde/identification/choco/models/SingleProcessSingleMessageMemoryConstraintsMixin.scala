4f8dac360fec3f1ad93a8cd42b200041e0cc582c
Rodolfo Jordão <jordao@kth.se>
2022-08-31 22:43:42 +0200

    adding more

---
+++ choco/src/main/scala/idesyde/identification/choco/models/SingleProcessSingleMessageMemoryConstraintsMixin.scala
@@ -0,0 +1,31 @@
+package idesyde.identification.choco.models
+
+import idesyde.identification.choco.interfaces.ChocoModelMixin
+import org.chocosolver.solver.variables.IntVar
+
+trait SingleProcessSingleMessageMemoryConstraintsMixin extends ChocoModelMixin {
+
+  def processSize: Array[Int]
+  def dataSize: Array[Int]
+
+  def processesMemoryMapping: Array[IntVar]
+  def messagesMemoryMapping: Array[IntVar]
+  def memoryUsage: Array[IntVar]
+
+  def memories = 0 until memoryUsage.size
+
+  def postManyProcessManyMessageMemoryConstraints(): Unit = {
+    memories.foreach(mem => {
+      // val pMaps = processesMemoryMapping.map(p => p(mem).asIntVar())
+      // val cMaps = messagesMemoryMapping.map(c => c(mem).asIntVar())
+      chocoModel
+        .scalar(
+          processesMemoryMapping ++ messagesMemoryMapping,
+          processSize ++ dataSize,
+          "<=",
+          memoryUsage(mem)
+        )
+        .post()
+    })
+  }
+}

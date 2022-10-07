d5bc1c9214b24a534833551fcdf2f60d6c36bed2
Rodolfo Jordoa <jordao@kth.se>
2022-10-07 13:50:17 +0200

    Added a solution limiter

--- scala-cli/src/main/scala/idesyde/IDeSyDeStandalone.scala
+++ scala-cli/src/main/scala/idesyde/IDeSyDeStandalone.scala
@@ -7,6 +7,8 @@ import idesyde.identification.IdentificationHandler
 import idesyde.exploration.ExplorationHandler
 import forsyde.io.java.core.ForSyDeSystemGraph
 import scala.concurrent.ExecutionContext
+import scribe.format.FormatterInterpolator
+import scribe.Level
 
 object IDeSyDeStandalone {
 
@@ -17,9 +19,39 @@ object IDeSyDeStandalone {
       IDeSyDeRunConfig(executionContext = ExecutionContext.global)
     ) match {
       case Some(runConfig) =>
-        runConfig.run()
+        setLoggingLevel(Level.get(runConfig.verbosityLevel).getOrElse(Level.Info))
+        runConfig.copy(
+          debugLogger = (s) => scribe.debug(s),
+          infoLogger = (s) => scribe.info(s),
+          warnLogger = (s) => scribe.warn(s),
+          errorLogger = (s) => scribe.error(s)
+        ).run()
       case _ =>
 
     }
+
+  def setLoggingLevel(loggingLevel: Level) =
+    if (loggingLevel == Level.Debug)
+      scribe.Logger.root
+        .clearHandlers()
+        .clearModifiers()
+        .withHandler(
+          minimumLevel = Some(loggingLevel),
+          formatter =
+            formatter"${scribe.format.dateFull} [${scribe.format.levelColoredPaddedRight}] ${scribe.format
+              .italic(scribe.format.classNameSimple)} - ${scribe.format.messages}"
+        )
+        .replace()
+    else
+      scribe.Logger.root
+        .clearHandlers()
+        .clearModifiers()
+        .withHandler(
+          minimumLevel = Some(loggingLevel),
+          formatter =
+            formatter"${scribe.format.dateFull} [${scribe.format.levelColoredPaddedRight}] ${scribe.format.messages}"
+        )
+        .replace()
+    scribe.info(s"logging levels set to ${loggingLevel.name}.")
 
 }
--- scala-cli/src/main/scala/idesyde/cli/IDeSyDeRunConfig.scala
+++ scala-cli/src/main/scala/idesyde/cli/IDeSyDeRunConfig.scala
@@ -3,7 +3,6 @@ package idesyde.cli
 import java.nio.file.Path
 import java.nio.file.Paths
 import scala.collection.mutable.Buffer
-import scribe.Level
 import forsyde.io.java.drivers.ForSyDeModelHandler
 import idesyde.identification.IdentificationHandler
 import idesyde.exploration.ExplorationHandler
@@ -15,7 +14,6 @@ import idesyde.identification.forsyde.ForSyDeDecisionModel
 import idesyde.exploration.forsyde.interfaces.ForSyDeIOExplorer
 import idesyde.identification.forsyde.api.ForSyDeIdentificationModule
 import idesyde.identification.minizinc.api.MinizincIdentificationModule
-import scribe.format.FormatterInterpolator
 import idesyde.identification.DecisionModel
 
 case class IDeSyDeRunConfig(
@@ -23,36 +21,34 @@ case class IDeSyDeRunConfig(
     var outputModelPath: Path = Paths.get("idesyde-result.forsyde.xml"),
     var allowedDecisionModels: Buffer[String] = Buffer(),
     var verbosityLevel: String = "INFO",
+    var solutionLimiter: Int = 0,
+    val debugLogger: (String) => Unit = (s) => {},
+    val infoLogger: (String) => Unit = (s) => {},
+    val warnLogger: (String) => Unit = (s) => {},
+    val errorLogger: (String) => Unit = (s) => {},
     executionContext: ExecutionContext
 ) {
 
-  val explorationHandler = ExplorationHandler(
-    infoLogger = (s: String) => scribe.info(s),
-    debugLogger = (s: String) => scribe.debug(s)
-  )
+  val explorationHandler = ExplorationHandler(infoLogger = infoLogger, debugLogger = debugLogger)
     .registerModule(ChocoExplorationModule())
 
-  val identificationHandler = IdentificationHandler(
-    infoLogger = (s: String) => scribe.info(s),
-    debugLogger = (s: String) => scribe.debug(s)
-  )
+  val identificationHandler = IdentificationHandler(infoLogger = infoLogger, debugLogger = debugLogger)
     .registerIdentificationRule(ChocoIdentificationModule())
     .registerIdentificationRule(ForSyDeIdentificationModule())
     .registerIdentificationRule(MinizincIdentificationModule())
 
   def run(): Unit =
-    setLoggingLevel(Level.get(verbosityLevel).getOrElse(Level.Info))
     val modelHandler = ForSyDeModelHandler()
     val validInputs =
       inputModelsPaths.map(f => (f, modelHandler.canLoadModel(f)))
     if (validInputs.forall((p, b) => !b)) {
-      scribe.error(
+      errorLogger(
         "At least one valid model is necessary"
       )
     } else if (validInputs.exists((p, b) => !b)) {
-      scribe.error("These inputs are invalid (unknown format): " + validInputs.filter((p, b) => b).map((p, b) => p.getFileName()).mkString(", "))
+      errorLogger("These inputs are invalid (unknown format): " + validInputs.filter((p, b) => b).map((p, b) => p.getFileName()).mkString(", "))
     } else {
-      scribe.info("Reading and merging input models.")
+      infoLogger("Reading and merging input models.")
       val model = validInputs
         .map((p, _) => modelHandler.loadModel(p))
         .foldLeft(ForSyDeSystemGraph())((merged, m) =>
@@ -61,18 +57,20 @@ case class IDeSyDeRunConfig(
         )
 
       val identified = identificationHandler.identifyDecisionModels(model)
-      scribe.info(s"Identification finished with ${identified.size} decision model(s).")
+      infoLogger(s"Identification finished with ${identified.size} decision model(s).")
       if (identified.size > 0)
         val chosen = explorationHandler.chooseExplorersAndModels(identified)
         val chosenFiltered =
           if (allowedDecisionModels.size > 0) then
             chosen.filter((exp, dm) => allowedDecisionModels.contains(dm.uniqueIdentifier))
           else chosen
-        scribe.info(s"Total of ${chosenFiltered.size} combo of decision model(s) and explorer(s) chosen.")
+        infoLogger(s"Total of ${chosenFiltered.size} combo of decision model(s) and explorer(s) chosen.")
         // identified.foreach(m => m match {
         //   case mzn: MiniZincForSyDeDecisionModel => scribe.debug(s"mzn model: ${mzn.mznInputs.toString}")
         // })
-        //var numSols = 0
+        if (chosenFiltered.size > 1) {
+          warnLogger(s"Taking a random decision model and explorer combo.")
+        }
         val numSols = chosenFiltered.headOption
           .filter((e, decisionModel) =>
             e.isInstanceOf[ForSyDeIOExplorer] && decisionModel.isInstanceOf[ForSyDeDecisionModel]
@@ -81,50 +79,26 @@ case class IDeSyDeRunConfig(
           .map((explorer, decisionModel) =>
             explorer
               .explore(decisionModel)(using executionContext)
-              .foldLeft(0)((res, result) => {
+              .scanLeft(0)((res, result) => {
                 if (!outputModelPath.toFile.exists || outputModelPath.toFile.isFile) then
-                  scribe.debug(s"writing solution at ${outputModelPath.toString}")
+                  debugLogger(s"writing solution at ${outputModelPath.toString}")
                   modelHandler.writeModel(model.merge(result), outputModelPath)
                 else if (outputModelPath.toFile.exists && outputModelPath.toFile.isDirectory) then
-                  val outPath = outputModelPath.resolve(Paths.get(s"solution_${res.toString}"))
-                  scribe.debug(s"writing solution at ${outPath.toString}")
+                  val outPath = outputModelPath.resolve(Paths.get(s"solution_${res.toString}.fiodl"))
+                  debugLogger(s"writing solution at ${outPath.toString}")
                   modelHandler.writeModel(
                     model.merge(result),
-                    outputModelPath.resolve(Paths.get(s"solution_${res.toString}"))
+                    outputModelPath.resolve(Paths.get(s"solution_${res.toString}.fiodl"))
                   )
                 res + 1
-              })
+              }).takeWhile(res => (solutionLimiter <= 0) || (solutionLimiter > 0 && res <= solutionLimiter)).last
           )
           .getOrElse(0)
         if (numSols > 0)
-          scribe.info(s"Finished exploration with ${numSols} solution(s)")
+          infoLogger(s"Finished exploration with ${numSols} solution(s)")
         else
-          scribe.info(s"Finished exploration with no solution")
+          infoLogger(s"Finished exploration with no solution")
       //scribe.info("Finished successfully")
     }
-
-  def setLoggingLevel(loggingLevel: Level) =
-    if (loggingLevel == Level.Debug)
-      scribe.Logger.root
-        .clearHandlers()
-        .clearModifiers()
-        .withHandler(
-          minimumLevel = Some(loggingLevel),
-          formatter =
-            formatter"${scribe.format.dateFull} [${scribe.format.levelColoredPaddedRight}] ${scribe.format
-              .italic(scribe.format.classNameSimple)} - ${scribe.format.messages}"
-        )
-        .replace()
-    else
-      scribe.Logger.root
-        .clearHandlers()
-        .clearModifiers()
-        .withHandler(
-          minimumLevel = Some(loggingLevel),
-          formatter =
-            formatter"${scribe.format.dateFull} [${scribe.format.levelColoredPaddedRight}] ${scribe.format.messages}"
-        )
-        .replace()
-    scribe.info(s"logging levels set to ${loggingLevel.name}.")
 
 }

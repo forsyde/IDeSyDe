8d209a344108c37e8606f7608d7207414b579c2e
GuoguoXiaomiao <yuhanz@kth.se>
2023-01-31 11:09:19 +0100

    sdf and periodic task

---
+++ scala-common/src/main/scala/idesyde/identification/common/models/mixed/SDFandTask.scala
@@ -0,0 +1,18 @@
+package idesyde.identification.common.models.mixed
+
+import idesyde.identification.common.StandardDecisionModel
+import idesyde.identification.common.models.sdf.SDFApplication
+import idesyde.identification.common.models.workload.{CommunicatingExtendedDependenciesPeriodicWorkload, InstrumentedWorkloadMixin}
+import spire.math.Rational
+
+final case class SDFandTask(
+                     sdf:SDFApplication,
+                     task:CommunicatingExtendedDependenciesPeriodicWorkload,
+                     sdfperiod: Vector[Rational],
+                     totalperiod: Vector[Rational],
+                     ) extends StandardDecisionMode with InstrumentedWorkloadMixin{
+  val coverElements=sdf.coveredElements ++ task.coveredElements
+  val coveredElementRelations=sdf.coveredElementRelations ++ task.coveredElementRelations
+  val messagesMaxSizes=sdf.messagesMaxSizes ++ task.messagesMaxSizes
+  val uniqueIdentifier: String = "SDFandTask"
+}

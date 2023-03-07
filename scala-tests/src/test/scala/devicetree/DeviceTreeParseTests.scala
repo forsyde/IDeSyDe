1e651820d3a7796067d9d6fb771eec3a584e2a8d
Rodolfo Jordão <jordao@kth.se>
2023-03-07 23:04:35 +0100

    DeviceTree even more robust

--- scala-tests/src/test/scala/devicetree/DeviceTreeParseTests.scala
+++ scala-tests/src/test/scala/devicetree/DeviceTreeParseTests.scala
@@ -12,19 +12,43 @@ class DeviceTreeParseTests
     with CanParseDeviceTree {
 
   val inlinedCode = """
+    chassis-type = "embedded"
+    compatible = "custom,custom,partitioned"
     cpus {
       ublaze1: cpu@0 {
-        timebase-frequency = <50000000>
+        clock-frequency = <50000000>
+
+        ops-per-cycle {
+          default {
+            f64add = <1 6>
+            f64mul = <1 1000>
+          }
+        }
       }
     }
 
     memory@0x00000 {
-        
+        clock-frequency = <50000000>
+        device-type = "memory"
+        reg = <0x0 0x800000>
+    }
+
+    bus {
+      compatible = "simple-bus"
+      arbitration = "fair"
+      
     }
     """
 
   test("Trying to parse the most basic ones") {
-    val parser = parseDeviceTree(inlinedCode)
-    println(parser.toString)
+    val root = parseDeviceTree(inlinedCode)
+    root match
+      case Success(result, next) =>
+        println(result)
+        result.cpus.foreach(cpu => println(cpu.operationsProvided))
+        result.memories.foreach(mem => println(mem.memorySize))
+      case Failure(msg, next) => println(msg)
+      case Error(msg, next)   => println(msg)
+
   }
 }

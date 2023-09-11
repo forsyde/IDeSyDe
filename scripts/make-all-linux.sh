7a580446a36960676cff4b1e72c0269e88c1841c
Rodolfo Jordao <jordao@kth.se>
2023-09-11 22:30:35 +0200

    GH 11

--- scripts/make-all-linux.sh
+++ scripts/make-all-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sbt publishModules
 ./gradlew publishModules
-cargo build --release --target x86_64-unknown-linux-musl
-cp ./target/x86_64-unknown-linux-musl/release/idesyde-orchestrator idesyde
+cargo build --release
+cp ./target/release/idesyde-orchestrator idesyde
 zip -r idesyde-linux-musl-x86_64.zip idesyde imodules emodules

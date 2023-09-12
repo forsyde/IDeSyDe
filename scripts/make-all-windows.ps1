bf971f6789ada727d564c0dbeaa311734a343119
Rodolfo Jordao <jordao@kth.se>
2023-09-12 13:39:45 +0200

    Updated scripts

--- scripts/make-all-windows.ps1
+++ scripts/make-all-windows.ps1
@@ -2,4 +2,4 @@ sbt publishModules
 .\gradlew.bat publishModules
 cargo build --release
 cp .\target\release\idesyde-orchestration.exe idesyde.exe
-zip -r idesyde-windows-msvc-x86_64.zip idesyde.exe imodules emodules
+zip -r idesyde-x86_64-pc-windows-msvc.zip idesyde.exe imodules emodules

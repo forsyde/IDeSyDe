a05564c9b84c2f15f379288c61c6db0eb395d898
jordao <jordao@kth.se>
2023-09-12 10:58:30 +0200

    Mistake in convenience script

--- scripts/make-all-linux.sh
+++ scripts/make-all-linux.sh
@@ -2,5 +2,5 @@
 sbt publishModules
 ./gradlew publishModules
 cargo build --release
-cp ./target/release/idesyde-orchestrator idesyde
+cp ./target/release/idesyde-orchestration idesyde
 zip -r idesyde-linux-gnu-x86_64.zip idesyde imodules emodules

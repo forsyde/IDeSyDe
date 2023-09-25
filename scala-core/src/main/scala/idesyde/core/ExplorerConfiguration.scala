2228c127ea2a284fb2d99fdc07c74ace1750bb93
jordao <jordao@kth.se>
2023-09-25 13:27:53 +0200

    Forgot scala code

---
+++ scala-core/src/main/scala/idesyde/core/ExplorerConfiguration.scala
@@ -0,0 +1,10 @@
+package idesyde.core
+
+import upickle.default._
+
+final case class ExplorerConfiguration(
+    max_sols: Long = -1L,
+    total_timeout: Long = -1L,
+    time_resolution: Long = -1L,
+    memory_resolution: Long = -1L
+) derives ReadWriter {}

e0321640164bf1bb2b6fcdb78bd0a66dd416a8aa
Rodolfo Jordao <jordao@kth.se>
2024-04-22 15:16:15 +0200

    modifying the decision model for better modularity

--- java-common/src/main/java/idesyde/common/HardwareImplementationArea.java
+++ java-common/src/main/java/idesyde/common/HardwareImplementationArea.java
@@ -6,16 +6,19 @@ import idesyde.core.DecisionModel;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
  * A decision model to hold the required area that a hardware implementation needs.
  */
 public record HardwareImplementationArea(
-        @JsonProperty("required_areas") Map<String, Long> requiredAreas
+    Set<String> processes,
+    @JsonProperty("programmable_areas") Set<String> programmableAreas,
+    @JsonProperty("required_areas") Map<String, Map<String, Long>> requiredAreas
 ) implements DecisionModel {
     @Override
     public Set<String> part() {
-        return Set.of();
+        return Stream.concat(processes.stream(), programmableAreas.stream()).collect(Collectors.toSet());
     }
 }
